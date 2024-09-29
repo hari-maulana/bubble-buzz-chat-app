7da74439f9e8230f19d34357b7085974d3e7d622
maulanahari <harimaulana.xyz@gmail.com>
2024-09-30 00:52:53 +0700

    (f-login): add styling an initial UI

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,39 @@
 import "./Login.css";
+// @ts-ignore
+import assets from "../../assets/assets";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="login">
+      <img src={assets.logo_big} alt="logo" />
+      <form action="" className="login-form">
+        <h2>Sign Up</h2>
+        <input
+          type="text"
+          placeholder="username"
+          className="form-input"
+          required
+        />
+        <input
+          type="email"
+          placeholder="email"
+          className="form-input"
+          required
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="form-input"
+          required
+        />
+        <button type="submit">Sign Up</button>
+        <div className="login-term">
+          <input type="checkbox" />
+          <p>Agree to terms of use & privacy policy.</p>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
