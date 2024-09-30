a1c9dc561550f84d1067334e2f9c6b19c4de9cea
maulanahari <harimaulana.xyz@gmail.com>
2024-09-30 23:40:45 +0700

    (left-sidebar): add initial ui

--- src/component/LeftSIdebar/LeftSidebar.tsx
+++ src/component/LeftSIdebar/LeftSidebar.tsx
@@ -1,6 +1,32 @@
 import "./LeftSideBar.css";
+// @ts-ignore
+import assets from "../../assets/assets";
 const LeftSidebar = () => {
-  return <div>LeftSidebar</div>;
+  return (
+    <div className="ls">
+      <div className="ls-top">
+        <div className="ls-nav">
+          <img src={assets.logo} alt="" className="logo" />
+          <div className="menu">
+            <img src={assets.menu_icon} alt="" />
+          </div>
+        </div>
+        <div className="ls-search">
+          <img src={assets.search_icon} alt="" />
+          <input type="text" placeholder="search here" />
+        </div>
+      </div>
+      <div className="ls-list">
+        <div className="friends">
+          <img src={assets.profile_img} alt="" />
+          <div>
+            <p>Gaga Richard</p>
+            <span>online meeeeee</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LeftSidebar;
