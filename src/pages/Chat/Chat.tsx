9a584fd51fe94987f9f6465368c4f15e44cff763
maulanahari <harimaulana.xyz@gmail.com>
2024-09-30 23:05:45 +0700

    (chat-page): mounting component

--- src/pages/Chat/Chat.tsx
+++ src/pages/Chat/Chat.tsx
@@ -1,10 +1,15 @@
+import ChatBox from "../../component/Chatbox/ChatBox";
+import LeftSidebar from "../../component/LeftSIdebar/LeftSidebar";
+import RightSideBar from "../../component/RightSidebar/RightSideBar";
 import "./Chat.css";
 
 const Chat = () => {
   return (
     <div className="chat">
       <div className="chat-container"></div>
-      HI
+      <LeftSidebar />
+      <ChatBox />
+      <RightSideBar />
     </div>
   );
 };
