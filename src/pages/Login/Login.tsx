8bab8c833628e7a1d02bdefee8dea970d3b437f0
maulanahari <harimaulana.xyz@gmail.com>
2024-09-30 11:07:45 +0700

    (login): add register options

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,19 +1,24 @@
 import "./Login.css";
 // @ts-ignore
 import assets from "../../assets/assets";
+import { useState } from "react";
 
 const Login = () => {
+  const [currState, setCurrState] = useState("Login");
+
   return (
     <div className="login">
       <img className="login-logo" src={assets.logo_big} alt="logo" />
       <form action="" className="login-form">
-        <h2>Sign Up</h2>
-        <input
-          type="text"
-          placeholder="username"
-          className="form-input"
-          required
-        />
+        <h2>{currState}</h2>
+        {currState === "Sign up" && (
+          <input
+            type="text"
+            placeholder="username"
+            className="form-input"
+            required
+          />
+        )}
         <input
           type="email"
           placeholder="email"
@@ -26,15 +31,30 @@ const Login = () => {
           className="form-input"
           required
         />
-        <button type="submit">Sign Up</button>
-        <div className="login-term">
-          <input type="checkbox" />
-          <p>Agree to terms of use & privacy policy.</p>
-        </div>
+        {currState === "Sign up" ? (
+          <button type="submit">Sign Up</button>
+        ) : (
+          <button type="submit">Login now</button>
+        )}
+
+        {currState === "Sign up" && (
+          <div className="login-term">
+            <input type="checkbox" />
+            <p>Agree to terms of use & privacy policy.</p>
+          </div>
+        )}
         <div className="login-forgot">
-          <p className="login-toggle">
-            Already have an account? <span>click here</span>
-          </p>
+          {currState === "Sign up" ? (
+            <p className="login-toggle">
+              Already have an account?{" "}
+              <span onClick={() => setCurrState("Login")}>click here</span>
+            </p>
+          ) : (
+            <p className="login-toggle">
+              Don't have an account?{" "}
+              <span onClick={() => setCurrState("Sign up")}>create here</span>
+            </p>
+          )}
         </div>
       </form>
     </div>
