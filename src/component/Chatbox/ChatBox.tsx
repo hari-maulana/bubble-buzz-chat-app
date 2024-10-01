3a3e002d15a77af6debdfd3c16d520a9ad0c222e
maulanahari <harimaulana.xyz@gmail.com>
2024-10-01 22:54:03 +0700

    (chatBox): add chat content receiver and sender

--- src/component/Chatbox/ChatBox.tsx
+++ src/component/Chatbox/ChatBox.tsx
@@ -12,6 +12,24 @@ const ChatBox = () => {
         </p>
         <img src={assets.help_icon} alt="" />
       </div>
+
+      <div className="chat-message">
+        <div className="s-message">
+          <p className="message">Lorem ipsum, dolor sit amet ...</p>
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p className="time">10:30 AM</p>
+          </div>
+        </div>
+        <div className="r-message">
+          <p className="message">Lorem ipsum, dolor sit amet ...</p>
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p className="time">10:30 AM</p>
+          </div>
+        </div>
+      </div>
+
       <div className="chat-input">
         <input type="text" placeholder="Send your message" />
         <input type="file" id="image" accept="image/png, image/jpeg" hidden />
