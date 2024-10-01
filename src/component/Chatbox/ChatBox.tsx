a78065e8e536ac1a15b0e37d5333de95f95b9944
maulanahari <harimaulana.xyz@gmail.com>
2024-10-01 22:15:42 +0700

    (chat-box): initial ui for chat-box

--- src/component/Chatbox/ChatBox.tsx
+++ src/component/Chatbox/ChatBox.tsx
@@ -1,7 +1,19 @@
 import "./ChatBox.css";
+// @ts-ignore
+import assets from "../../assets/assets";
 
 const ChatBox = () => {
-  return <div>ChatBox</div>;
+  return (
+    <div className="chat-box">
+      <div className="chat-user">
+        <img src={assets.profile_img} alt="" />
+        <p>
+          Gaga Richard <img src={assets.green_dot} alt="" />
+        </p>
+        <img src={assets.help_icon} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default ChatBox;
