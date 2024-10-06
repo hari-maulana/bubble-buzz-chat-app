18c17ae30fdf7a253e35ba5e188caff3d21265a1
maulanahari <harimaulana.xyz@gmail.com>
2024-10-06 23:07:16 +0700

    Initial UI for profile page

--- src/pages/ProfileUpdate/ProfileUpdate.tsx
+++ src/pages/ProfileUpdate/ProfileUpdate.tsx
@@ -1,7 +1,26 @@
 import "./ProfileUpdate.css";
+// @ts-ignore
+import assets from "../../assets/assets";
 
 const ProfileUpdate = () => {
-  return <div>ProfileUpdate</div>;
+  return (
+    <div className="profile">
+      <div className="profile container">
+        <form>
+          <h3>Profile Details</h3>
+          <label htmlFor="avatar">
+            <input type="file" id="avatar" accept=".jpg, .jpeg, .png" hidden />
+            <img src={assets.avatar_icon} alt="" />
+            upload profile image
+          </label>
+          <input type="text" placeholder="your name" required />
+          <textarea placeholder="Write your bio" id=""></textarea>
+          <button>Save</button>
+        </form>
+        <img src={assets.logo_icon} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default ProfileUpdate;
